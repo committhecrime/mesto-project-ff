4876f82a858506a432c8240eec702bc2e4817bc6
Artur Makutonin <makutonin@Arturs-MacBook-Air.local>
2023-10-23 21:41:58 +0200

    fix: according to the 1 code review

--- scripts/index.js
+++ scripts/index.js
@@ -3,42 +3,41 @@ const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
 const cardContainer = document.querySelector('.places__list')
 // @todo: Функция создания карточки
-function cardCreate(cardArray) {
-  
 
-  cardArray.forEach(card => {
-    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-    const cardImage = cardElement.querySelector('.card__image');
-    const cardTitle = cardElement.querySelector('.card__title');
+function createSingleCard(card){
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-    cardImage.src = card.link;
-    cardImage.alt = card.name;
-    cardTitle.textContent = card.name;
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
 
-    isLiked(cardElement);
-    deleteCard(cardElement);
-    renderCard(cardElement);
-  });
-  
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
+  cardTitle.textContent = card.name;
+
+  function deleteCard(cardElement) {
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', function(){
+      cardElement.remove();
+    });
+  }
+  // @todo: Вывести карточки на страницу
+  function renderCard(cardElement) {
+    cardContainer.append(cardElement);
+  }
+
+  deleteCard(cardElement);
+  renderCard(cardElement);
 }
+
+
+function createCardsFromArray(cards) {
+  cards.forEach(card => {
+    createSingleCard(card);
+  });
+}
+
 // @todo: Функция удаления карточки
-function deleteCard(cardElement) {
-  const deleteButton = cardElement.querySelector('.card__delete-button');
-  deleteButton.addEventListener('click', function(){
-    cardElement.remove();
-  });
-}
-// @todo: Вывести карточки на страницу
-function renderCard(cardElement) {
-  cardContainer.append(cardElement);
-}
-// like button functionality
-function isLiked(cardElement) {
-  const likeButton = cardElement.querySelector('.card__like-button');
-  likeButton.addEventListener('click', function() {
-    likeButton.classList.toggle('card__like-button_is-active')
-  });
-}
 
-cardCreate(initialCards);
+
+createCardsFromArray(initialCards);
