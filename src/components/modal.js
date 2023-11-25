e6df6f4b70b56216a62863acfaf252672deeb3ca
Artur Makutonin <makutonin@Arturs-MacBook-Air.local>
2023-11-25 21:34:25 +0300

    vse4

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,12 +1,12 @@
 import { closeByEscape } from "../index.js";
 
-function openModal(popUp) {
-  popUp.classList.add("popup_is-opened");
+function openModal(popup) {
+  popup.classList.add("popup_is-opened");
   document.addEventListener("keydown", closeByEscape);
 }
 
-function closeModal(popUp) {
-  popUp.classList.remove("popup_is-opened");
+function closeModal(popup) {
+  popup.classList.remove("popup_is-opened");
   document.addEventListener("keydown", closeByEscape);
 }
 
