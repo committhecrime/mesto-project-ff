df08732399fa1dc8218ab2a8600d282dbd9217fa
Artur Makutonin <makutonin@Arturs-MacBook-Air.local>
2023-12-22 23:20:46 +0100

    third part: first try - fetch training

--- src/components/card.js
+++ src/components/card.js
@@ -1,29 +1,79 @@
-import { cardImagePopUp, cardPopUpCaption, cardTemplate } from "../index.js";
+import { cardImagePopUp, cardPopUpCaption, cardTemplate, onDelete } from "../index.js";
 import { openModal } from "./modal";
 import { cardPopUp } from "../index.js";
 
-function createSingleCard(card, deleteCard, likeCard, openCard) {
+function createSingleCard(card, deleteCard, likeCard, openCard, userId) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   const cardImage = cardElement.querySelector(".card__image");
   const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  const like = cardElement.querySelector(".card__like-button");
+  const likeElement = document.querySelector(".card__like-number");
 
+
+  
+  cardElement.id = card._id;
   cardImage.src = card.link;
   cardImage.alt = card.name;
   cardTitle.textContent = card.name;
+  let likes = card.likes;
 
-  deleteCard(cardElement);
+ 
+  
+  function deleteCard() {
+    cardElement.remove();
+  };
+/*
+  function checkCardLikes () {
+    return likes.some((like) => like._id === userId);
+  }
+
+
+    const setLikeStatus = () => {
+      const isLiked = checkCardLikes();
+      if (isLiked) {
+        like.classList.add("card__like-button_is-active");
+      } else {
+        like.classList.remove("card__like-button_is-active");
+      }
+      likeElement.textContent = likes.length;
+    };
+
+    setLikeStatus();
+
+    */
+
   likeCard(cardElement);
   openCard(cardElement);
+
+
+    function checkDeleteButton() {
+      if(card.hasOwnProperty("owner")){
+        if (card.owner._id !== userId) {
+        deleteButton.style.visibility = "hidden";
+      }
+      }
+    }
+
+  checkDeleteButton();
+
+  deleteButton.addEventListener('click', () => {
+    onDelete(card._id, deleteCard)
+  })
+
 
   return cardElement;
 }
 
 function deleteCard(cardElement) {
-  const deleteButton = cardElement.querySelector(".card__delete-button");
+  
   deleteButton.addEventListener("click", function () {
     cardElement.remove();
   });
 }
+
+
+
 
 function likeCard(cardElement) {
   const like = cardElement.querySelector(".card__like-button");
--- src/index.js
+++ src/index.js
@@ -17,12 +17,22 @@ const formElement = document.forms[0];
 const nameInput = document.querySelector(".popup__input_type_name");
 const jobInput = document.querySelector(".popup__input_type_description");
 const profileName = document.querySelector(".profile__title");
+
 const profileJob = document.querySelector(".profile__description");
 const formElementAdd = document.forms[1];
 const linkInput = document.querySelector(".popup__input_type_url");
 const cardNameInput = document.querySelector(".popup__input_type_card-name");
 export const openedPopUp = document.querySelector(".popup_is-opened");
 export const popUp = document.querySelector(".popup");
+const popUpDelete = document.querySelector(".popup_type_delete");
+const deleteButton = document.querySelector(".card__delete-button");
+
+const formAvatar = document.forms[2];
+const editAvatar = document.querySelector(".profile__image");
+const popupAvatarProfile = document.querySelector(".popup_type_avatar-edit");
+const avatarInput = document.querySelector(".popup__input_type_avatar");
+let userId = '';
+let userAvatar = '';
 
 function renderCard(cards) {
   cards.forEach((card) =>
@@ -43,6 +53,8 @@ editButton.addEventListener("click", function () {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 });
+
+
 
 newCardButton.addEventListener("click", function () {
   openModal(newCardPopUp);
@@ -76,14 +88,17 @@ export function closeByEscape(evt) {
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
+  setupUser({ name: nameInput.value, about: jobInput.value })
+  .then(() => {
+      profileName.textContent = nameInput.value;
+      profileJob.textContent = jobInput.value;
 
-  const name = nameInput.value;
-  const job = jobInput.value;
-
-  profileName.textContent = name;
-  profileJob.textContent = job;
-
-  closeModal(formElement.closest(".popup"));
+      evt.target.reset();
+      
+      closeModal(formElement.closest(".popup"));
+  })
+  .catch((err) => console.log(err))
+  
 }
 
 function handleFormSubmitAdd(evt) {
@@ -92,23 +107,257 @@ function handleFormSubmitAdd(evt) {
   const cardLink = linkInput.value;
   const cardName = cardNameInput.value;
 
-  const newCard = {
-    name: cardName,
-    link: cardLink,
-  };
+  addCardServer({ name: cardName, link: cardLink })
+    .then((data) => {
+      const newCard = createSingleCard(
+        data,
+        deleteCard,
+        likeCard,
+        openCard,
+        userId
+      );
+      cardNameInput.value = "";
+      linkInput.value = "";
+      cardContainer.prepend(newCard);
 
-  const newRenderCard = createSingleCard(
-    newCard,
-    deleteCard,
-    likeCard,
-    openCard
-  );
-  cardContainer.prepend(newRenderCard);
+      evt.target.reset();
 
-  formElementAdd.reset();
+      closeModal(formElementAdd.closest(".popup"));
+    })
+    .catch((err) => console.log(err));
 
-  closeModal(formElementAdd.closest(".popup"));
 }
 
 formElement.addEventListener("submit", handleFormSubmit);
 formElementAdd.addEventListener("submit", handleFormSubmitAdd);
+
+
+//VALIDATION
+
+const showError = (form, input, error) => {
+  const errorElement = form.querySelector(`.${input.id}-error`);
+  
+  errorElement.textContent = error; 
+  input.classList.add("popup__input_type_error");
+  errorElement.classList.add("popup__error_visible");
+
+};
+
+const hideError = (form, input) => {
+  const errorElement = form.querySelector(`.${input.id}-error`);
+  
+  input.classList.remove("popup__input_type_error");
+  errorElement.classList.remove("popup__error_visible");
+  errorElement.textContent = "";
+};
+
+const checkInputValidity = (form, input) => {
+
+  if (input.validity.patternMismatch) {
+    input.setCustomValidity(input.dataset.errorMessage);
+  } else {
+    input.setCustomValidity("");
+  }
+
+
+  if (!input.validity.valid) {
+    showError(form, input, input.validationMessage);
+  } else {
+    hideError(form, input);
+  }
+};
+
+const setEventListeners = (formElement) => {
+
+  const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
+  const submitButtonSelector = formElement.querySelector(".popup__button");
+
+  toggleButtonState(inputList, submitButtonSelector);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, submitButtonSelector);
+    });
+  });
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+}; 
+
+const toggleButtonState = (inputList, submitButtonSelector) => {
+  if (hasInvalidInput(inputList)) {
+    submitButtonSelector.disabled = true;
+    submitButtonSelector.classList.add("popup__button_disabled");
+  } else {
+    submitButtonSelector.disabled = false;
+    submitButtonSelector.classList.remove("popup__button_disabled");
+  }
+}; 
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll(".popup__form"));
+
+  formList.forEach((formElement) => {
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation();
+
+
+
+function handleFormAvatar(evt) {
+  evt.preventDefault();
+  updateAvatar({ avatar: avatarInput.value })
+    .then((data) => {
+      console.log(data.avatar)
+      editAvatar.style = `background-image: url(${data.avatar})`;
+      userAvatar = data.avatar;
+      closePopup(popupAvatarProfile);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    
+}const openPopupAvatar = function () {
+  openModal(popupAvatarProfile);
+  avatarInput.value = userAvatar;
+};
+
+formAvatar.addEventListener('click', handleFormAvatar)
+
+export const onDelete = (id, deleteCard) => {
+  removeCard(id)
+    .then((data) => deleteCard(data))
+
+    .catch((err) => console.log(err));
+};
+
+
+const config = {
+  baseUrl: "https://mesto.nomoreparties.co/v1/wff-cohort-2",
+  headers: {
+    authorization : "f5f541a4-74fb-4c46-bd96-8b596c9ba44e",
+    "Content-Type": "application/json",
+  },
+};
+
+export const getInitialCards = () => {
+  return fetch(`${config.baseUrl}/cards`, {
+    headers: config.headers,
+  }).then((res) => {
+    if(res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  });
+};
+
+export const getDataUser = () => {
+  return fetch(`${config.baseUrl}/users/me`, {
+    headers: config.headers,
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  });
+};
+
+export const setupUser = (data) => {
+  return fetch(`${config.baseUrl}/users/me`, {
+    method: "PATCH",
+    headers: config.headers,
+    body: JSON.stringify(data),
+  });
+};
+
+export const removeCard = (id) => {
+  return fetch(`${config.baseUrl}/cards/${id}`, {
+    method: "DELETE",
+    headers: config.headers,
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  });
+}
+
+export const getLikeCard = () => {
+  return fetch(`${config.baseUrl}/cards/`, {
+    headers: config.headers,
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  });
+};
+
+
+
+
+export const addCardServer = (data) => {
+  return fetch(`${config.baseUrl}/cards`, {
+    method: "POST",
+    headers: {
+      authorization: "f5f541a4-74fb-4c46-bd96-8b596c9ba44e",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  });
+};
+
+
+export const updateAvatar = (data) => {
+  return fetch("https://nomoreparties.co/v1/wff-cohort-2/users/me/avatar", {
+    method: "PATCH",
+    headers: {
+      authorization: "f5f541a4-74fb-4c46-bd96-8b596c9ba44e",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  });
+};
+
+/*function changeAvatarBg (evt) {
+  console.log(evt.target.style)
+  evt.target.classList.toggle("hover-background");
+}*/
+
+
+editAvatar.addEventListener("click", openPopupAvatar);
+
+
+Promise.all([getInitialCards(), getDataUser()])
+  .then(([cards, userData]) => {
+    userId = userData._id;
+    userAvatar = userData.avatar;
+    profileName.textContent = userData.name;
+    profileJob.textContent = userData.about;
+    editAvatar.style.backgroundImage = `url(${userData.avatar})`;
+
+    cards.forEach((data) => {
+      const userCard = createSingleCard(data, deleteCard, likeCard, openCard, userId);
+      cardContainer.prepend(userCard);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
